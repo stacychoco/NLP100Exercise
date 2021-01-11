7f02b7ea1c06893ddc95a9db1a02de1321179964
Stacy Nguyen <thaiha2905@gmail.com>
2021-01-10 20:25:53 -0500

    created text file for uk article

--- chapter3_regex/nlp_ch3.py
+++ chapter3_regex/nlp_ch3.py
@@ -8,6 +8,7 @@ Chapter 3: Regular Expression
 Author: Stacy Nguyen
 """
 
+from os import path
 import json
 
 
@@ -41,6 +42,17 @@ def read_body(title, list):
     return None
 
 
+def write_to_file(file_name, body):
+    """
+    Creates a fiie and writes a body article to it.
+    :param file_name: name of the file to be written to
+    :param body: body of the article
+    """
+    if not path.exists(file_name):
+        with open(file_name, 'w') as file:
+            file.write(body)
+
+
 if __name__ == '__main__':
     """
     20. Read JSON documents
@@ -49,4 +61,4 @@ if __name__ == '__main__':
     """
     json = read_json('enwiki-country.json')
     uk_body = read_body("United Kingdom", json)
-    print(uk_body)
+    write_to_file("uk_article.txt", uk_body)
