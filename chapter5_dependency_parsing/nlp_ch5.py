c728942d8f9d82e0558bb976c7d891cef20b8b2d
Stacy Nguyen <thaiha2905@gmail.com>
2021-02-02 20:56:35 -0500

    Problem 41

--- chapter5_dependency_parsing/nlp_ch5.py
+++ chapter5_dependency_parsing/nlp_ch5.py
@@ -9,6 +9,7 @@ Author: Stacy Nguyen
 """
 
 import spacy
+from nltk.tokenize import sent_tokenize
 
 # Demo of spacy module:
 """
@@ -21,10 +22,13 @@ for token in doc:
 
 
 class Word:
-    def __init__(self, text, lemma="", pos=""):
+    def __init__(self, text, lemma="", pos="", head="", dep="", children=[]):
         self.text = text
         self.lemma = lemma
         self.pos = pos
+        self.head = head
+        self.dep = dep
+        self.children = children
 
 
 def load_parse_result(sentence):
@@ -42,7 +46,9 @@ def convert_sentence(sentence):
     data = load_parse_result(sentence)
     word_array = []
     for token in data:
-        word_array.append(Word(token.text, token.lemma_, token.pos_))
+        word_array.append(Word(token.text, token.lemma_, token.pos_,
+                               token.head, token.dep_,
+                               [child for child in token.children]))
     return word_array
 
 
@@ -53,11 +59,26 @@ if __name__ == '__main__':
     lemma (lemma), and pos (part-of-speech). Represent a sentence as an array of instances of Word class. 
     Implement a program to load the parse result, and store the text as an array of sentences. 
     Show the object of the first sentence of the body of the article.
+    
+    41. Read the parse result (dependency)
+    In addition to problem 40, add three member variables head (a reference to the object of its 
+    syntactic governor), dep (dependency type to its governor), and children (a list of references 
+    to the syntactic dependents in the parse tree) to the class Word. Show the pairs of governors 
+    (parents) and their dependents (children) of the first sentence of the body of the article. 
+    Use the class Word in the rest of the problems in this chapter.
     """
-    # This sentence is now represented as a list of Word objects
-    converted_sentence = convert_sentence("Sometimes, people become superheroes.")
-    for object in converted_sentence:
-        print(object.text, object.lemma, object.pos)
-
     # Represent article as a list of sentences
-    # Output the first sentence of the text
+    with open("ai.en/ai.en.txt", 'r') as file:
+        article = file.read()
+    tokenized_article = sent_tokenize(article)
+
+    # Output the first sentence of the text as a list of objects
+    first_sentence = tokenized_article[0]
+
+    # This sentence is now represented as a list of Word objects
+    converted_sentence = convert_sentence(first_sentence)
+    for object in converted_sentence:
+        # problem 40: text, lemma, pos
+        # problem 41: head, dep, children
+        print(object.text, object.lemma, object.pos,
+              object.head, object.dep, object.children)
